0cc0a0c22dcaf807f03817fadc8bb836789fb611
ALLANOSAN <allanstryper@live.com>
2024-10-01 23:48:59 -0300

    alteração no codigo

--- Program.cs
+++ Program.cs
@@ -5,17 +5,22 @@ class Program
     static void Main()
     {
         // Variáveis para armazenar o nome e a quantidade de experiência (XP) do herói
+        string? nome;
+        
+
+        // Solicita o nome do herói
         Console.Write("Digite o nome do herói: ");
-        string? nome = Console.ReadLine();
+        nome = Console.ReadLine();
         if (nome == null)
         {
             Console.WriteLine("Nome não pode ser nulo.");
             return;
         }
 
+        // Solicita a quantidade de XP do herói
         Console.Write("Digite a quantidade de experiência (XP) do herói: ");
-        string? xpInput = Console.ReadLine();
-        if (xpInput == null || !int.TryParse(xpInput, out int xp))
+        string? xpEntrada = Console.ReadLine();
+        if (xpEntrada == null || !int.TryParse(xpEntrada, out int xp))
         {
             Console.WriteLine("XP inválido.");
             return;
@@ -24,32 +29,32 @@ class Program
         // Variável para armazenar o nível do herói
         string nivel;
 
-        // Estrutura de decisão para determinar o nível do herói com base na XP
+        // Estrutura de decisão para determinar o nível do herói com base no XP
         if (xp < 1000)
         {
             nivel = "Ferro";
         }
-        else if (xp <= 2000)
+        else if (xp >= 1001 && xp <= 2000)
         {
             nivel = "Bronze";
         }
-        else if (xp <= 5000)
+        else if (xp >= 2001 && xp <= 5000)
         {
             nivel = "Prata";
         }
-        else if (xp <= 7000)
+        else if (xp >= 5001 && xp <= 7000)
         {
             nivel = "Ouro";
         }
-        else if (xp <= 8000)
+        else if (xp >= 7001 && xp <= 8000)
         {
             nivel = "Platina";
         }
-        else if (xp <= 9000)
+        else if (xp >= 8001 && xp <= 9000)
         {
             nivel = "Ascendente";
         }
-        else if (xp <= 10000)
+        else if (xp >= 9001 && xp <= 10000)
         {
             nivel = "Imortal";
         }
@@ -58,7 +63,7 @@ class Program
             nivel = "Radiante";
         }
 
-        // Exibindo a mensagem final
+        // Exibe a mensagem com o nome e o nível do herói
         Console.WriteLine($"O Herói de nome {nome} está no nível de {nivel}");
     }
 }
